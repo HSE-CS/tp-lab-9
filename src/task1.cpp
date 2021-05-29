a4d2db641a3877d6bd49e473237a04771f7ad453
NikDemoShow <Nixon2001@yandex.ru>
2021-05-29 14:01:44 +0300

    first push

---
+++ src/task1.cpp
@@ -0,0 +1,11 @@
+// Copyright NikDemoShow 2021
+#include"task1.h"
+void Bsort(std::vector<std::string>& objects, const std::function<bool(std::string, std::string)> comp) {
+	for (size_t i{ 0 }; i < objects.size(); ++i) {
+		for (size_t j{ 0 }; j < objects.size(); ++j) {
+			if (comp(objects[i], objects[j])) {
+				std::swap(objects[i], objects[j]);
+			}
+		}
+	}
+}
---
+++ src/task2.cpp
@@ -0,0 +1,21 @@
+// Copyright NikDemoShow 2021
+#include"task2.h"
+void Bsort(std::vector<std::string>& objects,
+	const std::function<bool(std::string, std::string)> comp,
+	bool& done, std::mutex& mu,
+	std::condition_variable& cv) {
+	for (size_t i{ 0 }; i < objects.size(); ++i) {
+		for (size_t j{ 0 }; j < objects.size(); ++j) {
+			if (comp(objects[i], objects[j])) {
+				std::lock_guard <std::mutex> lg(mu);
+				std::swap(objects[i], objects[j]);
+			}
+			cv.notify_all();
+		}
+	}
+	{
+		std::lock_guard<std::mutex> lg(mu);
+		done = true;
+	}
+	cv.notify_all();
+}
