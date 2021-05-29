6d2935ce47d1948e8fe29c378c3b5e1ca73fd8b9
AlexeusC4ever <62253233+AlexeusC4ever@users.noreply.github.com>
2021-05-29 18:16:54 +0300

    Add files via upload

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,5 +1,56 @@
-// Copyright 2021 GHA Test Team
+//  Copyright Baklanov 2021
+#include "../include/task1.h"
 #include <gtest/gtest.h>
-#include "task1.h"
-#include "task2.h"
-#include "task3.h"
+#include <string>
+#include <vector>
+#include <iostream>
+
+TEST(Sort, TEST1) {
+    std::vector<std::string> products = { "milk",
+        "orange", "bread", "meat", "fish" };
+    bubblesort(products, [](std::string a, std::string b)
+        {return a.length() > b.length(); });
+    std::vector<std::string> answer = { "milk",
+        "meat", "fish", "bread", "orange" };
+    EXPECT_EQ(answer, v);
+}
+
+TEST(Sort, TEST2) {
+    std::vector<std::string> products = { "milk",
+        "orange", "bread", "meat", "fish" };
+    bubblesort(products, [](std::string a, std::string b)
+        {return a.length() < b.length(); });
+    std::vector<std::string> answer = { "orange",
+        "bread", "milk", "meat", "fish" };
+    EXPECT_EQ(answer, v);
+}
+
+TEST(Sort, TEST3) {
+    std::vector<std::string> products = { "milk",
+        "orange", "bread", "meat", "fish" };
+    bubblesort(products, [](std::string a, std::string b)
+        {return a < b; });
+    std::vector<std::string> answer = { "orange",
+        "milk", "meat", "fish", "bread" };
+    EXPECT_EQ(answer, v);
+}
+
+TEST(Sort, TEST4) {
+    std::vector<std::string> products = { "milk",
+        "orange", "bread", "meat", "fish" };
+    bubblesort(products, [](std::string a, std::string b)
+        {return a > b; });
+    std::vector<std::string> answer = { "bread",
+        "fish", "meat", "milk", "orange" };
+    EXPECT_EQ(answer, v);
+}
+
+TEST(Sort, TEST5) {
+    std::vector<std::string> products = { "milk",
+        "orange", "bread", "meat", "fish" };
+    bubblesort(products, [](std::string a, std::string b)
+        {return a[0] < b[0]; });
+    std::vector<std::string> answer = { "orange",
+        "meat", "milk", "fish", "bread" };
+    EXPECT_EQ(answer, v);
+}
