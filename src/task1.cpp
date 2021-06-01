efcacdf53a13d2fed497dbc796a5c2a27d2819ae
HabKaffee <neknaum1@gmail.com>
2021-06-01 18:51:51 +0300

    Fix in bubble sort

--- src/task1.cpp
+++ src/task1.cpp
@@ -3,9 +3,9 @@
 
 void stringBubbleSort(std::vector<std::string>& strVector, std::function<bool(std::string, std::string)> f) {
     for (size_t i = 0; i < strVector.size() - 1; ++i) {
-        for (size_t j = i + 1; j < strVector.size(); ++j) {
-            if (f(strVector[i], strVector[j])) {
-                std::swap(strVector[i], strVector[j]);
+        for (size_t j = 0; j < strVector.size() - i - 1; ++j) {
+            if (f(strVector[j], strVector[j+1])) {
+                std::swap(strVector[j], strVector[j+1]);
             }
         }
     }
