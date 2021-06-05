0b1966a8eb4e510df71218963bd3e9375afc5647
Your Name <olga.frolova.2001@bk.ru>
2021-06-05 12:11:27 +0300

    task1

--- include/task2.h
+++ include/task2.h
@@ -1,10 +1,1 @@
 // Copyright 2021 FOM
-#ifndef INCLUDE_TASK2_H_
-#define INCLUDE_TASK2_H_
-#include <chrono>
-#include <condition_variable>
-#include <functional>
-#include <iostream>
-#include <mutex>
-#include <string>
-// Copyright 2021 FOM
