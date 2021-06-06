9d26063d4eb12205c8f19147f910231601bf3ea2
MalininDD <54958481+MalininDD@users.noreply.github.com>
2021-06-06 22:21:47 +0300

    fix

--- src/main3.cpp
+++ src/main3.cpp
@@ -1,27 +1,27 @@
-// Copyright 2021 MalininDmitry
-
-#include <iostream>
-#include <vector>
-#include <functional>
-#include <string>
-#include "../include/task3.h"
-#include <mutex>
-#include <thread>
-#include <queue>
-
-
-int main() {
-    int max_queue = 5;
-    std::vector<int> person1 = {1,3,5,6};
-    std::vector<int> person2 = {1,5,6};
-    std::vector<int> person3 = {1,3,5,6,6,4,};
-    std::vector<int> person4 = {1,3,5,6};
-    std::vector<int> person5 = {1,3};
-    std::vector<int> person6 = {1};
-    std::vector<int> person7 = {1,334,2,11,12,4,4,5,3,2};
-    std::vector<std::vector<int>> persons = {person1,
-        person2, person3, person4, person5, person6, person7};
-
-    shop3(persons, max_queue);
-    return 0;
-}
+//// Copyright 2021 MalininDmitry
+//
+//#include <iostream>
+//#include <vector>
+//#include <functional>
+//#include <string>
+//#include "../include/task3.h"
+//#include <mutex>
+//#include <thread>
+//#include <queue>
+//
+//
+//int main() {
+//    int max_queue = 5;
+//    std::vector<int> person1 = {1,3,5,6};
+//    std::vector<int> person2 = {1,5,6};
+//    std::vector<int> person3 = {1,3,5,6,6,4,};
+//    std::vector<int> person4 = {1,3,5,6};
+//    std::vector<int> person5 = {1,3};
+//    std::vector<int> person6 = {1};
+//    std::vector<int> person7 = {1,334,2,11,12,4,4,5,3,2};
+//    std::vector<std::vector<int>> persons = {person1,
+//        person2, person3, person4, person5, person6, person7};
+//
+//    shop3(persons, max_queue);
+//    return 0;
+//}
--- src/task1.cpp
+++ src/task1.cpp
@@ -1,16 +1,1 @@
 // Copyright 2021 MalininDmitry
-
-#include "task1.h"
-
-template<typename T>
-void sorttask(std::vector<T> &arr, const std::function<bool(T, T)>& comp) {
-    for (int i = 0; i < arr.size()-1; i++) {
-        for (int j = 0; j < arr.size() - i-1; j++) {
-            if (comp(arr[j] , arr[j+1])) {
-                T a = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = a;
-            }
-        }
-    }
-}
