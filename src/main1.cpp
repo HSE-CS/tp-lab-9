c30b8e229c4e63528ff76ba9e1a593aaa1dc2143
Ivan Dobryaev <ivan.dobryaev@gmail.com>
2021-06-06 15:33:53 +0300

    smth wrong x2

--- src/main1.cpp
+++ src/main1.cpp
@@ -7,31 +7,35 @@ int main() {
   "I", "mix", "alcohol", "my", "body", "is", "like", "a", "blender"};
 
 
-  bubbleSort(Strings, [](std::string x, std::string y) {
-             return x[0] < y[0]; });
-  print(Strings);
-  bubbleSort(Strings, [](std::string x, std::string y) {
-             return x.back() < y.back(); });
-  print(Strings);
-  bubbleSort(Strings, [](std::string x, std::string y) {
-             return x[x.size() / 3] < y[y.size() / 3]; });
-  print(Strings);
-  bubbleSort(Strings, [](std::string x, std::string y) {
-             return x.size() < y.size(); });
-  print(Strings);
-  bubbleSort(Strings, [](std::string x, std::string y) {
-    std::reverse(x.begin(), x.end());
-    return x < y;
-  });
-  print(Strings);
-  bubbleSort(Strings, [](std::string x, std::string y) {
-    std::remove(x.begin(), x.end(), x[0]);
-    std::remove(y.begin(), y.end(), y[0]);
-    return x < y;
-  });
+  std::function<bool(std::string, std::string)> comp1 = [](std::string x, std::string y) {
+             return x[0] < y[0]; };
+  bubbleSort(Strings, comp1);
   print(Strings);
 
-  bubbleSort(Strings, [](std::string x, std::string y) { return x < y; });
+  std::function<bool(std::string, std::string)> comp2 = [](std::string x, std::string y) {
+             return x.back() < y.back(); };
+  bubbleSort(Strings, comp2);
+  print(Strings);
+
+  std::function<bool(std::string, std::string)> comp3 =  [](std::string x, std::string y) {
+             return x[x.size() / 3] < y[y.size() / 3]; };
+  bubbleSort(Strings, comp3);
+  print(Strings);
+
+  std::function<bool(std::string, std::string)> comp4 = [](std::string x, std::string y) {
+             return x.size() < y.size(); };
+  bubbleSort(Strings, comp4);
+  print(Strings);
+
+  std::function<bool(std::string, std::string)> comp5 = [](std::string x, std::string y) {
+    std::reverse(x.begin(), x.end());
+    return x < y;
+  };
+  bubbleSort(Strings, comp5);
+  print(Strings);
+
+  std::function<bool(std::string, std::string)> comp6 = [](std::string x, std::string y) { return x < y; };
+  bubbleSort(Strings, comp6);
   print(Strings);
 
   return 0;
