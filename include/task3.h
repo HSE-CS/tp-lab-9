69cfcae711e9b450f8b3f332598c5dc11d14fd1d
Ivan Dobryaev <ivan.dobryaev@gmail.com>
2021-06-06 18:44:37 +0300

    try to make 3th task

---
+++ include/task3.h
@@ -0,0 +1,14 @@
+// Copyright 2021 LongaBonga
+
+
+#ifndef INCLUDE_TASK3_H_
+#define INCLUDE_TASK3_H_
+
+#include <random>
+#include <deque>
+#include <vector>
+
+
+void service(std::deque<std::vector<int>*>* line_customers, int active);
+
+#endif  // INCLUDE_TASK3_H_
---
+++ src/main3.cpp
@@ -0,0 +1,27 @@
+// Copyright 2021 LongaBonga
+#include <thread>
+#include "task3.cpp"
+
+
+int main() {
+
+    int customers{ static_cast<int>(rand() % 100 + 100) };
+    int active = 0;
+    auto* line_customers = new std::deque<std::vector<int>*>{};
+
+    while (customers) {
+        auto *perches = new std::vector<int>(
+                static_cast<int>(rand() % 10 + 1), 0);
+        for (int i { 0 }; i < perches->size(); i++) {
+            perches->at(i) = rand() % 10;
+        }
+        line_customers->push_back(perches);
+        if (line_customers->size() == 5) {
+            std::thread th(service, line_customers, active++);
+            th.detach();
+            line_customers = new std::deque<std::vector<int>*>{};
+        }
+        usleep(rand() % 55000);
+        customers--;
+    }
+}
