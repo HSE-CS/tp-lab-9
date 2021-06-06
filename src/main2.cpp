487917cd35508d23594103d29e1d00ef3054ba51
Schterben <dsbarinov_1@edu.hse.ru>
2021-06-06 21:05:22 +0300

    in main2.cpp bubblesort function corrected

--- src/main2.cpp
+++ src/main2.cpp
@@ -1,9 +1,8 @@
 // Copyright 2021 DBarinov
-
 #include "task2.h"
 
 int main() {
 	std::vector<std::string> v_string = { "helllo", "world", "this", "is", "demo" };
-	bubbleSortt2(v_string, [](std::string& _str1, std::string& _str2) {
-		return _str1.length() > _str2.length(); });
+	bubbleSortt2(v_string, [](std::string _str1, std::string _str2)
+		{ return _str1.length() > _str2.length(); });
 }
