b59b2a71e80fc73962b311db848d0008853195bd
Your Name <olga.frolova.2001@bk.ru>
2021-06-06 21:34:55 +0300

    fix

--- include/task1.h
+++ include/task1.h
@@ -12,7 +12,8 @@
 #include <string>
 
 
-template <typename A, typename Te = typename std::iterator_traits<A>::value_type>
+template <typename A,
+    typename Te = typename std::iterator_traits<A>::value_type>
 void BubbleSort(A start_,
     A finish_, std::function<bool(const Te &, const Te &)> comp_) {
   for (int i = finish_ - start_ - 1; i > 0; i--) {
--- src/main2.cpp
+++ src/main2.cpp
@@ -2,9 +2,9 @@
 
 #include "task2.h"
 
-//int main() {
-//    std::vector<std::string> test_ = { "monday", "sunday",
-//                                    "saturday", "friday" , "wednesday" , "thursday", "tuesday" };
-//    BubbleSortParrallel(test_, [](const std::string& first_, const std::string& second_) {
-//        return first_.length() > second_.length(); });
-//}
+int main() {
+    std::vector<std::string> test_ = { "monday", "sunday",
+                                    "saturday", "friday" , "wednesday" , "thursday", "tuesday" };
+    BubbleSortParrallel(test_, [](const std::string& first_, const std::string& second_) {
+        return first_.length() > second_.length(); });
+}
