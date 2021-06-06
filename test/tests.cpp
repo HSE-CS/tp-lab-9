2531148066836418d9338391c2b5dc356fc46420
Alexey Trutnev <66214960+alexgiving@users.noreply.github.com>
2021-06-06 19:50:42 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -8,36 +8,36 @@
 TEST(task1, t_1) {
  std::vector<std::string> vector_elements = {"scapigerous",
   "distraint", "wheeled"};
-    bubbleSort(vec, [](std::string& s1, std::string& s2) {
+    bubbleSort(vector_elements, [](std::string& s1, std::string& s2) {
         return s1 < s2; });
     std::vector<std::string> expect =
     { "distraint", "scapigerous", "wheeled"};
-    EXPECT_EQ(expect, vec);
+    EXPECT_EQ(expect, vector_elements);
 }
 
 TEST(task1, t_2) {
-    std::vector<std::string> vec = {"scapigerous", "distraint", "wheeled"};
-    bubbleSort(vec, [](std::string& s1, std::string& s2) {
+    std::vector<std::string> vector_elements = {"scapigerous", "distraint", "wheeled"};
+    bubbleSort(vector_elements, [](std::string& s1, std::string& s2) {
     return s1.size() < s2.size(); });
     std::vector<std::string> expect =
     {"wheeled", "distraint", "scapigerous"};
-    EXPECT_EQ(expect, vec);
+    EXPECT_EQ(expect, vector_elements);
 }
 
 TEST(task1, t_3) {
-    std::vector<std::string> vec = {"scapigerous", "distraint", "wheeled"};
-    bubbleSort(vec, [](std::string& s1, std::string& s2) {
+    std::vector<std::string> vector_elements = {"scapigerous", "distraint", "wheeled"};
+    bubbleSort(vector_elements, [](std::string& s1, std::string& s2) {
     return s1.back() < s2.back(); });
     std::vector<std::string> expect =
     {"wheeled", "scapigerous", "distraint"};
-    EXPECT_EQ(expect, vec);
+    EXPECT_EQ(expect, vector_elements);
 }
 
 TEST(task1, t_4) {
-    std::vector<std::string> vec = {"scapigerous", "distraint", "wheeled"};
-  bubbleSort(vec, [](std::string& s1, std::string& s2) {
+    std::vector<std::string> vector_elements = {"scapigerous", "distraint", "wheeled"};
+  bubbleSort(vector_elements, [](std::string& s1, std::string& s2) {
     return s1[0] < s2[0]; });
     std::vector<std::string> expect =
     { "distraint", "scapigerous", "wheeled"};
-    EXPECT_EQ(expect, vec);
+    EXPECT_EQ(expect, vector_elements);
 }
