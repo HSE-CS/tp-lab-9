ec2b679d75a5a62ba64b5d31ddf95db197e851a9
burgundypie <71260322+burgundypie@users.noreply.github.com>
2021-06-08 22:06:40 +0300

    Change vector`s name from array to strs

--- test/tests.cpp
+++ test/tests.cpp
@@ -10,27 +10,28 @@ using namespace std;
 
 TEST(task1, sort1){
 
-    vector<string> array;
+    vector<string> strs;
 
-    array.push_back("Short");
-    array.push_back("the long one");
-    array.push_back("middle");
+    strs.push_back("Short");
+    strs.push_back("the long one");
+    strs.push_back("middle");
 
-    BubbleSort(array, [] (const string& a, const string& b){ return a.size() > b.size();});
+    BubbleSort(strs, [] (const string& a, const string& b){ return a.size() > b.size();});
 
     EXPECT_EQ("middle", array[1]);
 
 }
 
 TEST(task2, sort2){
+    vector <string> strs;
 
-    array.push_back("C short");
-    array.push_back("G the long one");
-    array.push_back("A middle");
+    strs.push_back("C short");
+    strs.push_back("G the long one");
+    strs.push_back("A middle");
 
-    ThreadSort(array, [] (const string& a, const string& b){ return a[0] > b[0];});
+    ThreadSort(strs, [] (const string& a, const string& b){ return a[0] > b[0];});
 
-    EXPECT_EQ("A middle", array[0]);
+    EXPECT_EQ("A middle", strs[0]);
 }
 
 TEST(task3, shop1){
