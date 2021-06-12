a348be46a434e3cca72c822e005fa666e6ec1eb0
Karcevamd <mashkarcev@gmail.com>
2021-06-12 19:02:49 +0300

    19-PMI KARTSEVA

---
+++ include/task3.h
@@ -0,0 +1,43 @@
+//  Copyright 2021 by Kartseva Masha
+#pragma once
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <mutex>
+#include <queue>
+#include <chrono>
+#define TASK3_H
+#ifdef TASK3_H
+extern const int count_cust;
+extern const int que_len;
+extern std::mutex mtx;
+
+class Customer {
+private:
+    std::vector<int> products;
+public:
+    Customer();
+    std::vector<int> getProds();
+};
+
+class Checkout {
+private:
+    int size = 0;
+    std::queue<Customer*> customers;
+public:
+    Checkout() {};
+    void addCust(Customer* cust);
+    int getsize();
+    void serveCust();
+};
+
+
+class Shop {
+private:
+    std::vector<std::thread*> threads;
+    std::vector<Checkout*> checks;
+public:
+    Shop();
+    ~Shop();
+};
+#endif //  TASK3_H
---
+++ src/main3.cpp
@@ -0,0 +1,14 @@
+//  Copyright 2021 by Kartseva Masha
+#include <iostream>
+#include "task3.h"
+
+const int count_cust(18);
+const int que_len(5);
+std::mutex mtx;
+
+int main() {
+    Shop* magaz = new Shop();
+    magaz->~Shop();
+    system("pause");
+    return 0;
+}
