d5db5f5fc5c80dc9e6674823d2ecde5ab1a65b69
Karcevamd <mashkarcev@gmail.com>
2021-06-13 20:47:34 +0300

    sixth-fix

--- src/task1.cpp
+++ src/task1.cpp
@@ -5,7 +5,7 @@ bool comparator(const std::string& a, const std::string& b)) {
     std::vector<std::string>::iterator pos;
     for (size_t i = 0; i < arr.size() - 1; i++) {
         for (size_t j = 0; j < arr.size() - 1 - i; j++) {
-            if (comparator(arr[j], arr[j + 1])) swap(arr[j], arr[j + 1]);
+            if (comparator(arr[j], arr[j + 1])) std::swap(arr[j], arr[j + 1]);
         }
     }
     for (pos = arr.begin(); pos != arr.end(); pos++) {
